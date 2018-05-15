9e2bf60cc9c470da7d109f7a22c749795c125b30
John Livingston <jclivingston316@gmail.com>
2018-05-15 14:29:53 -0600

    user can click students name to get to show page

--- spec/features/students/user_can_see_a_list_of_students_spec.rb
+++ spec/features/students/user_can_see_a_list_of_students_spec.rb
@@ -12,4 +12,16 @@ describe 'user visits /students' do
     expect(page).to have_content(student_2.name)
     expect(page).to have_content(student_3.name)
   end
+
+  scenario 'click on students name to get to student show page' do
+    student_1 = Student.create!(name: 'Fred')
+    student_2 = Student.create!(name: 'Ted')
+    student_3 = Student.create!(name: 'Red')
+
+    visit "/students"
+
+    click_on "#{student_1.name}"
+
+    expect(current_path).to eq(student_path(student_1))
+  end
 end
