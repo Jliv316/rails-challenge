297d7fbfc847e416162e749f6f2d1651fcf8e0da
John Livingston <jclivingston316@gmail.com>
2018-05-15 14:09:57 -0600

    adds delete feature test

---
+++ spec/features/students/user_can_delete_a_student_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'user deletes an existing student' do
+  scenario 'a user can delete a student from students index' do
+    # message = 'Student was successfully deleted!'
+    student = Student.create!(name: "Thomas")
+    visit student_path(student)
+
+    click_link 'Delete'
+
+    # expect(page).to have_content(message)
+    expect(page).to have_content(student.name)
+    expect(page).to_not have_content("#{student.name}")
+  end
+end
